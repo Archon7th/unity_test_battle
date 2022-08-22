1658f5cc2c8282fe51fe2c9fc0d2e87955d880a3
Andrei Leonau <andrei.leonau@tinybuild.com>
2022-08-22 19:45:24 +0400

    Waypoint draft

---
+++ Assets/Scripts/Character/AI/Waypoint.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Assets.Scripts.GameBehaviors
+{
+    public class Waypoint : MonoBehaviour
+    {
+        [SerializeField] private List<Waypoint> m_pathes = new List<Waypoint>();
+        protected int steps;
+
+
+        private void Awake()
+        {
+            WaypointService.Instance.Register(this);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            PlayerCharacter player = collision.gameObject.GetComponent<PlayerCharacter>();
+            if (player)
+                WaypointService.Instance.PlayerTouch(this);
+
+            EnemySmartAI enemy = collision.gameObject.GetComponent<EnemySmartAI>();
+            if (enemy)
+                enemy.SetWaypoint(GetNextWaypoint());
+        }
+
+        public Waypoint GetNextWaypoint()
+        {
+            print($"GetNextWaypoint {steps}");
+            return m_pathes[Random.Range(0, m_pathes.Count)];
+        }
+
+        public void Track(int steps)
+        {
+            this.steps = steps;
+            foreach (Waypoint el in m_pathes)
+                if (el.steps < 0)
+                    el.Track(steps + 1);
+        }
+
+        public void Reset()
+        {
+            foreach (Waypoint el in m_pathes)
+                el.steps = -1;
+        }
+    }
+}
---
+++ Assets/Scripts/Character/AI/WaypointService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Assets.Scripts.GameBehaviors
+{
+    public class WaypointService : MonoBehaviour
+    {
+        public static WaypointService Instance { get; protected set; }
+
+        private readonly List<Waypoint> waypoints = new List<Waypoint>();
+
+        public WaypointService() : base()
+        {
+            Instance = this;
+        }
+
+        public void PlayerTouch(Waypoint waypoint)
+        {
+            print($"PlayerTouch {waypoint}");
+            foreach (Waypoint el in waypoints)
+                waypoint.Reset();
+
+            waypoint.Track(0);
+        }
+
+        public void Register(Waypoint waypoint)
+        {
+            if (!waypoints.Contains(waypoint))
+                waypoints.Add(waypoint);
+        }
+
+        public Waypoint GetClosestWaypoint(Vector3 position)
+        {
+            float bestRange = 0;
+            Waypoint bestMatched = null;
+            foreach (Waypoint el in waypoints)
+            {
+                float dist = Vector3.Distance(position, el.transform.position);
+                if (bestMatched == null || dist < bestRange)
+                {
+                    bestMatched = el;
+                    bestRange = dist;
+                }
+            }
+            return bestMatched;
+        }
+    }
+}
